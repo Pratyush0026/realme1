83b8686bcece49ea03feb0bbe39f5651bc67a7b6
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2023-12-05 20:31:56 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI2a0yhELCeC2Om1cKMIH9HCH0Hr3Jikc",
+  authDomain: "realme-9e5a8.firebaseapp.com",
+  projectId: "realme-9e5a8",
+  storageBucket: "realme-9e5a8.appspot.com",
+  messagingSenderId: "449939050554",
+  appId: "1:449939050554:web:002b9c3296e923d469e503"
+};
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
